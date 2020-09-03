835b6e7881645549b5681139e7626ecc0ba6786e
felipe.lima <felipena.lima@gmail.com>
2020-09-03 17:05:52 -0300

    feat: add heroku configs

---
+++ build/precache-manifest.18145ae4e07b1624be14128e3c5d05e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd2f379781b7b249479215edae57597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abb7d3399dfa8eb57d90",
+    "url": "/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "2d18e0a2606c2392c599",
+    "url": "/static/css/main.464df1a8.chunk.css"
+  },
+  {
+    "revision": "abb7d3399dfa8eb57d90",
+    "url": "/static/js/2.7039212d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7039212d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d18e0a2606c2392c599",
+    "url": "/static/js/main.9032d9fa.chunk.js"
+  },
+  {
+    "revision": "f3a586af524502814c7f",
+    "url": "/static/js/runtime-main.0f0c390f.js"
+  }
+]);
